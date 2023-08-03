e2e68af4f8bf4a1c4a7a48a8ce92bd152d95649a
Joseph Smith <joesmith40@gmail.com>
2023-08-03 16:48:51 -0400

    updated models

--- server/app.py
+++ server/app.py
@@ -1,32 +1,58 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
-# Standard library imports
+# # Standard library imports
 
-# Remote library imports
+# # Remote library imports
 
 
 from flask import request, make_response, session, jsonify, abort, render_template
 from flask_restful import Resource
 from werkzeug.exceptions import NotFound
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 import ipdb
 
-# Local imports
+# # Local imports
 from config import app, db, api, bcrypt
-from models import User, Butterfly, Plant, Tag
+from models import User, Grocery, Review, Order
 
 
 class Signup(Resource):
     def post(self):
         form_json = request.get_json()
-        new_user = User(username=form_json["username"], email=form_json["email"])
-        new_user.password_hash = form_json["password"]
-        db.session.add(new_user)
-        db.session.commit()
-        # Store the user ID in the session
-        session["user_id"] = new_user.id
+        email = form_json.get("email")
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            return {"error": "User already exists"}, 409
 
-        response = make_response(new_user.to_dict(), 201)
-        return response
+        try:
+            new_user = User(
+                email=form_json["email"],
+                first_name=form_json["first_name"],
+                last_name=form_json["last_name"],
+                address=form_json["address"],
+                phone_number=form_json["phone_number"],
+                is_admin=form_json["is_admin"],
+            )
+            new_user.password_hash = form_json["password"]
+            db.session.add(new_user)
+            db.session.commit()
+            # Store the user ID in the session
+            session["user_id"] = new_user.id
+
+            response = make_response(new_user.to_dict(), 201)
+            return response
+
+        except IntegrityError as e:
+            db.session.rollback()
+            if "UNIQUE constraint failed" in str(e.orig):
+                return {"error": "Email already associated with another user"}, 409
+            else:
+                return {"error": "An unexpected error has occurred"}, 500
+        except ValueError as e:
+            db.session.rollback()
+            return {"error": str(e)}, 422
+        finally:
+            db.session.close()
 
 
 api.add_resource(Signup, "/api/signup")
@@ -36,13 +62,13 @@ class Login(Resource):
     def post(self):
         json = request.get_json()
         try:
-            user = User.query.filter_by(username=json["username"]).first()
+            user = User.query.filter_by(email=json["email"]).first()
             if user.authenticate(json["password"]):
                 session["user_id"] = user.id
                 response = make_response(user.to_dict(), 200)
                 return response
         except:
-            abort(401, "Incorrect Username or Password")
+            return {"error": "Incorrect Email or Password"}, 401
 
 
 api.add_resource(Login, "/api/login")
@@ -65,123 +91,128 @@ class AuthorizedSessions(Resource):
             response = make_response(user.to_dict(), 200)
             return response
         except:
-            abort(401, "Unauthorized")
+            response = make_response("Unauthorized", 401)
+            return response
 
 
 api.add_resource(AuthorizedSessions, "/api/authorized")
 
 
-class Butterflies(Resource):
+class Groceries(Resource):
     def get(self):
-        if session["user_id"]:
-            butterfly_list = [b.to_dict() for b in Butterfly.query.all()]
-            response = make_response(
-                butterfly_list,
-                200,
-            )
-            return response
-
-        else:
-            response = make_response("Unauthorized", 401)
-            return response
+        grocery_list = [g.to_dict() for g in Grocery.query.all()]
+        response = make_response(
+            grocery_list,
+            200,
+        )
+        return response
 
     def post(self):
         form_json = request.get_json()
         try:
-            new_butterfly = Butterfly(
+            new_grocery = Grocery(
                 name=form_json["name"],
                 image=form_json["image"],
-                genus_species=form_json["genus_species"],
-                conservation_status=form_json["conservation_status"],
+                category=form_json["category"],
+                price=form_json["price"],
                 user_id=session["user_id"],
             )
+            db.session.add(new_grocery)
+            db.session.commit()
+
+            response = make_response(
+                new_grocery.to_dict(),
+                201,
+            )
+            return response
+
+        except IntegrityError as e:
+            db.session.rollback()
+            return {"error": "An unexpected error has occurred"}, 500
+
         except ValueError as e:
-            abort(422, e.args[0])
-
-        db.session.add(new_butterfly)
-        db.session.commit()
-
-        response = make_response(
-            new_butterfly.to_dict(),
-            201,
-        )
-        return response
+            db.session.rollback()
+            return {"error": str(e)}, 422
+        finally:
+            db.session.close()
 
 
-api.add_resource(Butterflies, "/api/butterflies")
+api.add_resource(Groceries, "/api/groceries")
 
 
-class ButterflyByID(Resource):
+class GroceryByID(Resource):
     def get(self, id):
-        butterfly = Butterfly.query.filter_by(id=id).first()
-        if not butterfly:
-            raise NotFound
-        response = make_response(butterfly.to_dict(), 200)
+        grocery = Grocery.query.filter_by(id=id).first()
+        if not grocery:
+            return {"error": "The item you are looking for cannot be found"}, 404
+        response = make_response(grocery.to_dict(), 200)
 
         return response
 
     def patch(self, id):
-        butterfly = Butterfly.query.filter_by(id=id).first()
-        if not butterfly:
-            raise NotFound
-        if butterfly.user_id != session["user_id"]:
-            abort(401, "Unauthorized")
+        grocery = Grocery.query.filter_by(id=id).first()
+        if not grocery:
+            return {"error": "The item you are looking for cannot be found"}, 404
+        user = User.query.get(session["user_id"])
+        if not user.is_admin:
+            return {"error": "You are not authorized to perform this action"}, 401
 
         form_json = request.get_json()
-        for attr in form_json:
-            setattr(butterfly, attr, form_json[attr])
 
-        db.session.add(butterfly)
-        db.session.commit()
+        try:
+            for attr in form_json:
+                setattr(grocery, attr, form_json[attr])
 
-        response = make_response(butterfly.to_dict(), 200)
-        return response
+            db.session.add(grocery)
+            db.session.commit()
+
+            response = make_response(grocery.to_dict(), 200)
+            return response
+
+        except IntegrityError as e:
+            db.session.rollback()
+            return {"error": "An unexpected error has occurred"}, 500
+
+        except ValueError as e:
+            db.session.rollback()
+            return {"error": str(e)}, 422
+        finally:
+            db.session.close()
 
     def delete(self, id):
-        butterfly = Butterfly.query.filter_by(id=id).first()
-        if not butterfly:
-            raise NotFound
-        if butterfly.user_id != session["user_id"]:
-            abort(401, "Unauthorized")
-        db.session.delete(butterfly)
+        grocery = Grocery.query.filter_by(id=id).first()
+        if not grocery:
+            return {"error": "The item you are looking for cannot be found"}, 404
+        user = User.query.get(session["user_id"])
+        if not user.is_admin:
+            return {"error": "You are not authorized to perform this action"}, 401
+
+        db.session.delete(grocery)
         db.session.commit()
 
-        response = make_response("Butterfly deleted", 204)
+        response = make_response("grocery deleted", 204)
 
         return response
 
 
-api.add_resource(ButterflyByID, "/api/butterflies/<int:id>")
-
-class ButterflyTags(Resource):
-    def get(self, n):
-        butterflies = [b for b in Butterfly.query.all()]
-        new_butterflies = [b.to_dict() for b in butterflies if len(b.tags) >= n]
-        response = make_response(
-            new_butterflies,
-            200
-        )
-        return response
-
-        # >=n
-
-api.add_resource(ButterflyTags, "/butterfly_tags/<int:n>")
+api.add_resource(GroceryByID, "/api/groceries/<int:id>")
 
 
-class Plants(Resource):
-    def get(self):
-        plant_list = [p.to_dict() for p in Plant.query.all()]
-        response = make_response(
-            plant_list,
-            200,
-        )
+class Reviews(Resource):
+    def get(self, id):
+        grocery = Grocery.query.get(id)
+        if not grocery:
+            return {"error": "The grocery item you are looking for cannot be found"}, 404
+
+        review_list = [r.to_dict() for r in grocery.reviews]
+        response = make_response(review_list, 200)
 
         return response
 
     def post(self):
         form_json = request.get_json()
         try:
-            new_plant = Plant(
+            new_review = Review(
                 name=form_json["name"],
                 genus_species=form_json["genus_species"],
                 image=form_json["image"],
@@ -201,93 +232,92 @@ class Plants(Resource):
         return response
 
 
-api.add_resource(Plants, "/api/plants")
+api.add_resource(Reviews, "/api/groceries/<int:id>/reviews")
 
 
-class PlantByID(Resource):
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first()
-        if not plant:
-            raise NotFound
-        response = make_response(plant.to_dict(), 200)
+# class PlantByID(Resource):
+#     def get(self, id):
+#         plant = Plant.query.filter_by(id=id).first()
+#         if not plant:
+#             raise NotFound
+#         response = make_response(plant.to_dict(), 200)
 
-        return response
+#         return response
 
-    def patch(self, id):
-        plant = Plant.query.filter_by(id=id).first()
-        if not plant:
-            raise NotFound
-        if plant.user_id != session["user_id"]:
-            abort(401, "Unauthorized")
-        else:
-            form_json = request.get_json()
-            for attr in form_json:
-                setattr(plant, attr, form_json[attr])
+#     def patch(self, id):
+#         plant = Plant.query.filter_by(id=id).first()
+#         if not plant:
+#             raise NotFound
+#         if plant.user_id != session["user_id"]:
+#             abort(401, "Unauthorized")
+#         else:
+#             form_json = request.get_json()
+#             for attr in form_json:
+#                 setattr(plant, attr, form_json[attr])
 
-            db.session.add(plant)
-            db.session.commit()
+#             db.session.add(plant)
+#             db.session.commit()
 
-            response = make_response(plant.to_dict(), 200)
-            return response
+#             response = make_response(plant.to_dict(), 200)
+#             return response
 
-    def delete(self, id):
-        plant = Plant.query.filter_by(id=id).first()
-        if not plant:
-            raise NotFound
-        if plant.user_id != session["user_id"]:
-            abort(401, "Unauthorized")
-        db.session.delete(plant)
-        db.session.commit()
+#     def delete(self, id):
+#         plant = Plant.query.filter_by(id=id).first()
+#         if not plant:
+#             raise NotFound
+#         if plant.user_id != session["user_id"]:
+#             abort(401, "Unauthorized")
+#         db.session.delete(plant)
+#         db.session.commit()
 
-        response = make_response("Plant Deleted", 204)
+#         response = make_response("Plant Deleted", 204)
 
-        return response
-
-
-api.add_resource(PlantByID, "/api/plants/<int:id>")
+#         return response
 
 
-class ButterflyTag(Resource):
-    def get(self, id):
-        butterfly = Butterfly.query.get(id)
-        if not butterfly:
-            response = make_response({"error": "Butterfly not found"}, 404)
-        else:
-            tag_list = [tag.to_dict() for tag in butterfly.tags]
-            response = make_response(tag_list, 200)
-        return response
-
-    def post(self, id):
-        json = request.get_json()
-        tag = Tag.query.filter_by(name=json["name"]).first()
-        if not tag:
-            tag = Tag(name=json["name"])
-        butterfly = Butterfly.query.filter_by(id=id).first()
-        butterfly.tags.append(tag)
-        db.session.add(butterfly)
-        db.session.commit()
-        response = make_response(butterfly.to_dict(), 201)
-        return response
+# api.add_resource(PlantByID, "/api/plants/<int:id>")
 
 
-api.add_resource(ButterflyTag, "/api/butterflies/<int:id>/tag")
+# class ButterflyTag(Resource):
+#     def get(self, id):
+#         butterfly = Butterfly.query.get(id)
+#         if not butterfly:
+#             response = make_response({"error": "Butterfly not found"}, 404)
+#         else:
+#             tag_list = [tag.to_dict() for tag in butterfly.tags]
+#             response = make_response(tag_list, 200)
+#         return response
+
+#     def post(self, id):
+#         json = request.get_json()
+#         tag = Tag.query.filter_by(name=json["name"]).first()
+#         if not tag:
+#             tag = Tag(name=json["name"])
+#         butterfly = Butterfly.query.filter_by(id=id).first()
+#         butterfly.tags.append(tag)
+#         db.session.add(butterfly)
+#         db.session.commit()
+#         response = make_response(butterfly.to_dict(), 201)
+#         return response
 
 
-# these will be all front end React unique routes
-@app.route("/")
-@app.route("/authentication")
-@app.route("/butterflies")
-@app.route("/butterflies/:id")
-@app.route("/butterflies/edit/:id")
-@app.route("/butterflies/new")
-@app.route("/plants")
-@app.route("/plants/:id")
-@app.route("/plants/new")
-@app.route("/addtothegarden")
-def index(id=0):
-    return render_template("index.html")
+# api.add_resource(ButterflyTag, "/api/butterflies/<int:id>/tag")
+
+
+# # these will be all front end React unique routes
+# @app.route("/")
+# @app.route("/authentication")
+# @app.route("/butterflies")
+# @app.route("/butterflies/:id")
+# @app.route("/butterflies/edit/:id")
+# @app.route("/butterflies/new")
+# @app.route("/plants")
+# @app.route("/plants/:id")
+# @app.route("/plants/new")
+# @app.route("/addtothegarden")
+# def index(id=0):
+#     return render_template("index.html")
 
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
-
--- server/models.py
+++ server/models.py
@@ -1,14 +1,14 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.hybrid import hybrid_property
+from better_profanity import profanity
 
 from config import db, bcrypt
 
-butterfly_tags = db.Table(
-    "butterfly_tags",
-    db.Column("butterfly_id", db.Integer, db.ForeignKey("butterflies.id")),
-    db.Column("tag_id", db.Integer, db.ForeignKey("tags.id")),
-    db.UniqueConstraint("butterfly_id", "tag_id", name="_butterfly_id_tag_id"),
+order_groceries = db.Table(
+    "order_groceries",
+    db.Column("grocery_id", db.Integer, db.ForeignKey("groceries.id")),
+    db.Column("order_id", db.Integer, db.ForeignKey("orders.id")),
 )
 
 
@@ -16,13 +16,36 @@ class User(db.Model, SerializerMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False, name="uq_username")
-    email = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    address = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
     _password_hash = db.Column(db.String)
-    butterflies = db.relationship("Butterfly", backref="user")
-    plants = db.relationship("Plant", backref="user")
+    reviews = db.relationship("Review", backref="user")
+    orders = db.relationship("Order", backref="user")
 
-    serialize_rules = ("-butterflies.user",)
+    serialize_rules = ("-reviews.user", "orders.user")
+
+    def validate_password(self, password):
+        password = password.strip()
+        if len(password) < 8:
+            raise ValueError("Password must be at least 8 characters long.")
+
+        if not any(char.isupper() for char in password):
+            raise ValueError("Password must contain at least one uppercase letter.")
+
+        if not any(char.islower() for char in password):
+            raise ValueError("Password must contain at least one lowercase letter.")
+
+        if not any(char.isdigit() for char in password):
+            raise ValueError("Password must contain at least one digit.")
+
+        if not any(char in '!@#$%^&*(),.?":{}|<>' for char in password):
+            raise ValueError(
+                'Password must contain at least one special character (!@#$%^&*(),.?":{}|<>).'
+            )
 
     @hybrid_property
     def password_hash(self):
@@ -36,61 +59,167 @@ class User(db.Model, SerializerMixin):
     def authenticate(self, password):
         return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
 
+    @validates("first_name")
+    def validate_first_name(self, key, first_name):
+        first_name = first_name.strip()
+        if not first_name:
+            raise ValueError("User must have a first name")
+        if len(first_name) > 50:
+            raise ValueError("First name must not exceed 50 characters")
+        if not first_name.isalpha():
+            raise ValueError("First name may only contain letters")
+        return first_name
+
+    @validates("last_name")
+    def validate_last_name(self, key, last_name):
+        last_name = last_name.strip()
+        if not last_name:
+            raise ValueError("User must have a last name")
+        if len(last_name) > 50:
+            raise ValueError("Last name must not exceed 50 characters")
+        if not last_name.isalpha():
+            raise ValueError("Last name may only contain letters")
+        return last_name
+
     @validates("email")
     def validate_email(self, key, email):
+        email = email.strip()
         if "@" not in email:
             raise ValueError("Please provide a valid email")
+        if "." not in email:
+            raise ValueError("Please provide a valid email")
         return email
+
+    @validates("phone_number")
+    def validate_phone(self, key, phone_number):
+        phone_number = phone_number.strip()
+        if len(phone_number) < 10:
+            raise ValueError("Phone number must be at least 10 characters long.")
+        if not phone_number.isdigit():
+            raise ValueError("Phone number may only contain numbers.")
+        return phone_number
 
     def __repr__(self):
         return f"<Name:{self.username}, Email:{self.email}>"
 
 
-class Butterfly(db.Model, SerializerMixin):
-    __tablename__ = "butterflies"
+class Grocery(db.Model, SerializerMixin):
+    __tablename__ = "groceries"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     image = db.Column(db.String, nullable=False)
-    genus_species = db.Column(db.String, nullable=False)
-    conservation_status = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    tags = db.relationship(
-        "Tag", secondary=butterfly_tags, back_populates="butterflies"
+    reviews = db.relationship("Review", backref="grocery")
+    orders = db.relationship(
+        "Order", secondary="order_groceries", back_populates="groceries"
     )
 
-    serialize_rules = ("-tags.butterflies", "-user.butterflies")
+    serialize_rules = ("-orders.groceries", "-user.groceries", "reviews.grocery")
+
+    ALLOWED_EXTENSIONS = ["png", "jpg", "jpeg", "gif"]
+
+    @validates("name")
+    def validates_name(self, key, name):
+        name = name.strip()
+        if not name:
+            raise ValueError("Grocery item must have a name")
+        if len(name) > 50:
+            raise ValueError("Name must not exceed 50 characters")
+        if not name.isalnum():
+            raise ValueError("Grocery names must contain only letters or numbers")
+        return name
+
+    @validates("image")
+    def validates_image(self, key, image):
+        image = image.strip()
+        if not image:
+            raise ValueError("Grocery item must have an image")
+        if "." not in image:
+            raise ValueError(
+                "Grocery image must have a valid extension (e.g., .png, .jpg, .jpeg, .gif)"
+            )
+        file_extension = image.rsplit(".", 1)[1].lower()
+        if file_extension not in Grocery.ALLOWED_EXTENSIONS:
+            raise ValueError(
+                "Invalid image file type. Allowed file types: png, jpg, jpeg, gif"
+            )
+        return image
+
+    @validates("category")
+    def validate_category(self, key, category):
+        category = category.strip()
+        if not category:
+            raise ValueError("Category must not be empty")
+        return category
+
+    @validates("price")
+    def validates_price(self, key, price):
+        price = price.strip()
+        if not price:
+            raise ValueError("Grocery must have a price")
+        dollars, cents = price.split(".")
+        if not dollars.isdigit() or not cents.isdigit() or len(cents) != 2:
+            raise ValueError("Price must be in valid format (e.g., 24.99, .89, 1.00)")
+        return price
 
     def __repr__(self):
-        return f"<Name:{self.name}>"
+        return f"<Name:{self.name}, Price:{self.price}>"
 
 
-class Plant(db.Model, SerializerMixin):
-    __tablename__ = "plants"
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    image = db.Column(db.String, nullable=False)
-    genus_species = db.Column(db.String, nullable=False)
-    growing_zone = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    stars = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    grocery_id = db.Column(db.Integer, db.ForeignKey("groceries.id"), nullable=False)
 
-    serialize_rules = ("-user.plants",)
+    serialize_rules = ("-user.reviews",)
+
+    @validates("content")
+    def validates_content(self, key, content):
+        content = content.strip()
+        if len(content) > 200:
+            raise ValueError("Reviews must be less than 200 characters")
+        if profanity.contains_profanity(content):
+            raise ValueError(
+                "Please refrain from using inappropriate language in reviews"
+            )
+        return content
+
+    @validates("stars")
+    def validates_stars(self, key, stars):
+        if not stars:
+            raise ValueError("Review must include star rating - 1 through 5")
+        return stars
 
     def __repr__(self):
-        return f"<Name:{self.name}, Genus/Species:{self.genus_species}, Growing Zone:{self.growing_zone}>"
+        return f"<Content:{self.content}, Stars:{self.stars}>"
 
 
-class Tag(db.Model, SerializerMixin):
-    __tablename__ = "tags"
+class Order(db.Model, SerializerMixin):
+    __tablename__ = "orders"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    butterflies = db.relationship(
-        "Butterfly", secondary=butterfly_tags, back_populates="tags"
+    items = db.Column(db.String, nullable=False)
+    total_items = db.Column(db.Integer, nullable=False)
+    tax = db.Column(db.Float, nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    groceries = db.relationship(
+        "Grocery", secondary="order_groceries", back_populates="orders"
     )
 
-    serialize_rules = ("-butterflies.tags",)
+    serialize_rules = ("-groceries.orders",)
+
+    @validates("items")
+    def validates_items(self, key, items):
+        if not items:
+            raise ValueError("Order must contain at least one item")
 
     def __repr__(self):
         return f"<Name:{self.name}>"
